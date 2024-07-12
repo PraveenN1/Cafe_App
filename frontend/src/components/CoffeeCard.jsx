c6d106bc4f49ba3c4fdd3b62b4fb0400f9032170
PraveenN1 <praveenpu46@gmail.com>
2024-07-12 17:58:05 +0530

    Toast-Notifications added

--- frontend/src/components/CoffeeCard.jsx
+++ frontend/src/components/CoffeeCard.jsx
@@ -4,32 +4,29 @@ import { CoffeeContext } from "./ApifetchExample";
 const CoffeeCard = ({ coffee }) => {
   const { title, image, price } = coffee;
   const { addOrder } = useContext(CoffeeContext);
-  const [toggleAdd,setToggleAdd]=useState(false);
   const [toggleClick,setToggleClick]=useState(0);
 
   const handleOrder = () => {
     addOrder(coffee);
-    setToggleAdd(!toggleAdd);
     setToggleClick(toggleClick+1);
   };
 
   return (
     <section className="flex flex-col items-center mt-2 p-4 rounded-md space-y-4 hover:border">
-      <div className="flex flex-col items-center space-y-4 rounded-md backdrop-blur-[6px]">
+      <div className="flex flex-col items-center space-y-4 rounded-md backdrop-blur-[6px] text-black ">
         <h1 className="text-center text-amber-300 font-semibold  text-2xl ">
           {title}
         </h1>
         <img src={image} alt="coffee" className="rounded-md" sync/>
-        <div className="flex w-full gap-4 ">
-          <button className="flex-1 h-10 rounded-md font-semibold text-lg border-2 bg-amber-400 hover:bg-amber-300">
+        <div className="flex w-full gap-2 flex-col md:flex-row">
+          <button className="flex-1 h-10 rounded-full font-semibold text-lg  bg-amber-400 hover:bg-amber-300">
             Rs {price}
           </button>
           <button
-            className="flex-1 h-10 rounded-md font-semibold text-lg border-2 bg-amber-600 text-white hover:bg-amber-700"
+            className="flex-1 h-10 rounded-full font-semibold text-lg  bg-amber-600 hover:bg-amber-700"
             onClick={handleOrder}
           >
             {toggleClick>0 ? `ADD (${toggleClick})`:"ADD"}
-            {console.log(toggleAdd)}
           </button>
         </div>
       </div>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,14 +2,21 @@ import React, { useContext, useState } from "react";
 import { CoffeeContext } from "../components/ApifetchExample";
 import CoffeeCard from "../components/CoffeeCard";
 import Modal from "../components/Modal";
-import axios from 'axios';
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
 
+import "react-toastify/dist/ReactToastify.css";
 
 const Home = () => {
   const { data, loading, orders, setOrders } = useContext(CoffeeContext);
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [isOpen,setIsOpen]=useState(false);
-  const [cancelModal,setCancelModal]=useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const [cancelModal, setCancelModal] = useState(false);
+
+  const notify = () =>
+    toast.success("Order placed successfully!", {
+      position: "top-center",
+    });
 
   const toggleModal = () => {
     setIsModalOpen(!isModalOpen);
@@ -38,27 +45,28 @@ const Home = () => {
 
   const placeOrder = async () => {
     try {
-      const orderItems = orders.map(order => ({
-        name: order.title,  
+      const orderItems = orders.map((order) => ({
+        name: order.title,
         price: order.price,
         image: order.image,
-        id: order.id
+        id: order.id,
       }));
-  
-      const response = await axios.post("https://cafe-app-backend-nine.vercel.app", {
-        orders: orderItems,
-        totalPrice,
-        gst,
-        finalPrice,
-      });
+
+      const response = await axios.post(
+        "https://cafe-app-backend-nine.vercel.app",
+        {
+          orders: orderItems,
+          totalPrice,
+          gst,
+          finalPrice,
+        }
+      );
       console.log("Order placed:", response.data);
       setOrders([]);
     } catch (error) {
       console.error("Error placing order:", error);
     }
   };
-  
-  
 
   const logOrders = () => {
     console.log("Current orders:", orders);
@@ -68,26 +76,28 @@ const Home = () => {
     placeOrder();
     logOrders();
     toggleModal();
+    notify();
   };
 
-  const handleCancelOrder=()=>{
+  const handleCancelOrder = () => {
     setOrders([]);
     setIsModalOpen(!toggleModal);
-  }
+  };
 
   return (
     <div className="px-10 overflow-hidden bg-black text-white">
-      <div className="flex  flex-row justify-center  text-nowrap overflow-hidden">
-        
-
-        <button className="w-auto rounded-full m-2 px-4 py-2 border-2 hover:bg-amber-600" onClick={toggleModal}>
+      <div className="flex flex-col sm:flex-row justify-center  text-nowrap overflow-hidden">
+        <button
+          className="w-auto rounded-full m-2 px-4 py-2 border-2 hover:bg-amber-600 hover:text-black hover:font-semibold"
+          onClick={toggleModal}
+        >
           Your Orders
         </button>
-        <button className="w-auto rounded-full m-2 px-4 py-2 border-2 hover:bg-amber-600">
+        <button className="w-auto rounded-full m-2 px-4 py-2 border-2 hover:bg-amber-600 hover:text-black hover:font-semibold">
           Added Items ({orders.length})
         </button>
       </div>
-      <div className="grid sm:grid-cols-2  md:grid-cols-4 lg:grid-cols-5 gap-4">
+      <div className="grid grid-cols-2  md:grid-cols-4 lg:grid-cols-5 gap-4">
         {loading ? (
           <p>Loading...</p>
         ) : (
@@ -97,71 +107,87 @@ const Home = () => {
         )}
       </div>
       <div className="">
-      <Modal isOpen={isModalOpen} onClose={toggleModal} >
-        <h2 className="text-3xl text-black font-semibold mb-4">Your Orders</h2>
-        <div className="flex flex-col">
-          <table className="w-full text-sm text-left rtl:text-right text-gray-600 dark:text-gray-400">
-            <tbody>
-              {orders.length > 0 ? (
-                orders.map((order, index) => (
-                  <tr key={index} className="mb-2">
-                    <td className="font-semibold text-xl">{order.title}</td>
-                    <td className="font-bold text-lg text-right">
-                      Rs {order.price}.00
-                    </td>
-                    <td>
-                      <button
-                        className="w-full text-3xl rounded-md"
-                        onClick={() => cancelOrder(index)}
-                      >
-                        &times;
-                      </button>
-                    </td>
-                  </tr>
-                ))
-              ) : (
-                <tr>
-                  <td colSpan="3">No orders yet.</td>
-                </tr>
-              )}
-              {orders.length > 0 && (
-                <>
+        <Modal isOpen={isModalOpen} onClose={toggleModal}>
+          <h2 className="text-3xl text-black font-semibold mb-4">
+            Your Orders
+          </h2>
+          <div className="flex flex-col">
+            <table className="w-full text-sm text-left rtl:text-right text-gray-600 dark:text-gray-400">
+              <tbody>
+                {orders.length > 0 ? (
+                  orders.map((order, index) => (
+                    <tr key={index} className="mb-2">
+                      <td className="font-semibold text-xl">{order.title}</td>
+                      <td className="font-bold text-lg text-right">
+                        Rs {order.price}.00
+                      </td>
+                      <td>
+                        <button
+                          className="w-full text-3xl rounded-md"
+                          onClick={() => cancelOrder(index)}
+                        >
+                          &times;
+                        </button>
+                      </td>
+                    </tr>
+                  ))
+                ) : (
                   <tr>
-                    <td className="font-bold text-black text-xl uppercase">Gst</td>
-                    <td className="font-bold text-xl text-right">Rs {gst}</td>
+                    <td colSpan="3">No orders yet.</td>
                   </tr>
-                  <tr>
-                    <td className="font-bold text-xl uppercase text-black">
-                      Total
-                    </td>
-                    <td className="font-bold text-xl text-black text-right">
-                      Rs {finalPrice}
-                    </td>
-                  </tr>
-                </>
-              )}
-            </tbody>
-          </table>
-          {orders.length > 0 && (
-            <div className="my-1 flex flex-col gap-1 font-semibold">
-              <button
-                className="w-full p-2 bg-yellow-400 hover:bg-yellow-500 rounded-md"
-                onClick={handlePlaceOrder}
-              >
-                Place Order
-              </button>
-              <button
-                className="w-full p-2 bg-red-400 hover:bg-red-500 rounded-md"
-                onClick={handleCancelOrder}
-              >
-                Cancel Order
-              </button>
-            </div>
-          )}
-        </div>
-      </Modal>
+                )}
+                {orders.length > 0 && (
+                  <>
+                    <tr>
+                      <td className="font-bold text-black text-xl uppercase">
+                        Gst
+                      </td>
+                      <td className="font-bold text-xl text-right">Rs {gst}</td>
+                    </tr>
+                    <tr>
+                      <td className="font-bold text-xl uppercase text-black">
+                        Total
+                      </td>
+                      <td className="font-bold text-xl text-black text-right">
+                        Rs {finalPrice}
+                      </td>
+                    </tr>
+                  </>
+                )}
+              </tbody>
+            </table>
+            {orders.length > 0 && (
+              <div className="my-1 flex flex-col gap-1 font-semibold">
+                <button
+                  className="w-full p-2 bg-yellow-400 hover:bg-yellow-500 rounded-md"
+                  onClick={handlePlaceOrder}
+                >
+                  Place Order
+                </button>
+                <button
+                  className="w-full p-2 bg-red-400 hover:bg-red-500 rounded-md"
+                  onClick={handleCancelOrder}
+                >
+                  Cancel Order
+                </button>
+              </div>
+            )}
+          </div>
+        </Modal>
       </div>
-
+      <ToastContainer
+        position="top-center"
+        autoClose={2000}
+        hideProgressBar
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+        transition:Flip
+      />
     </div>
   );
 };
